fa3471fca695b518abb7aa11cd4cb1f3b0ef1185
rnvllz <vaaron23@gmail.com>
2025-11-27 21:13:02 +0800

    progress i guess

---
+++ clothify/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => (
+  <nav className="bg-blue-600 p-4 text-white flex justify-between">
+    <Link to="/" className="font-bold text-xl">Clothing Store</Link>
+    <div className="space-x-4">
+      <Link to="/">Products</Link>
+      <Link to="/cart">Cart</Link>
+    </div>
+  </nav>
+);
+
+export default Navbar;
